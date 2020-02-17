e80c776d3c9ece5af468c23368104e54e1f21a56
[hickmanjv] <[hickmanjv@health.missouri.edu]>
2020-02-16 20:23:19 -0600

    Group8 Design Assignment
    Signed-off-by: Joshua Hickman <hickmanjv@health.missouri.edu>

---
+++ CS_4085 Python/RandomWriteRead/randomwrite.py
@@ -0,0 +1,19 @@
+"""
+Name: Joshua Hickman
+Pawprint: hickmanjv
+Assignment: Random Number File Writer and Reader
+Date: 02/15/20
+"""
+
+def main():
+    """
+
+
+"""
+
+    count = input(int("How many random numbers do you want? "))
+
+    lower_bound = input(int("What is the lowest the random number should be: "))
+    upper_bound = input(int("What is the highest the random number should be: "))
+    print("The random numbers were written to randomnum.txt")
+
