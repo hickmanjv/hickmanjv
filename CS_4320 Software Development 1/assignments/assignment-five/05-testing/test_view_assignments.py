1cfe7e9396006ee792bc7e341d2ec2f427f16fb4
[hickmanjv] <[hickmanjv@health.missouri.edu]>
2020-02-25 19:52:20 -0600

    cleaning up

--- CS_4320 Software Development 1/assignments/assignment-five/05-testing/test_view_assignments.py
+++ CS_4320 Software Development 1/assignments/assignment-five/05-testing/test_view_assignments.py
@@ -2,11 +2,17 @@ import pytest
 import System
 import Student
 
-# IMPORTANT - you have to run create assignment and create 'assignment5' for this
-#                        to check properly. The code only uses assignments from 'comp_sci'
-#                        but each course has only "assignment1" and "assignment2" by default
-#                        so it will always pass pytest unless you add and check an alternate
+# IMPORTANT - The code only uses assignments from 'comp_sci'
+#                         but each course has only "assignment1" and "assignment2" by default
+#                         so it will always pass pytest unless you add and check an alternate\
+#                         will add a section to create the assignment here
 def test_view_assignments(grading_system):
+    
+    #added to ensure there is the appropriate assignment tested for 
+    grading_system.login('goggins', 'augurrox')
+    grading_system.usr.create_assignment('assignment5', '03/10/20', 'databases')
+
+    # actual test
     grading_system.login('akend3', '123454321')
     grading_system.usr.view_assignments('databases')
     assert 'assignment5' in grading_system.usr.users['akend3']['courses']['databases']
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # hickmanjv
+
+This is the repository for all of my coursework thus far. It is constantly being updated  
+and will have new files each week/per assignment  
+
+I have unfortunately lost my C++ work for good.  
+
+Working 40hrs a week and taking a full time class load doesn't lend itself to having much free time  
+for other projects, however, they will be added once any are started/completed
+
+One of my projects will be staying private and I am currently working on a couple of patents  
+related to it and some other ideas
